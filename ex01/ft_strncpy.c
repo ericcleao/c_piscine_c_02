e8a6f12fcc250b17611fd1f7d25d4c8662d1cea3
Eric Cerqueira Leao <ecerquei@student.42sp.org.br>
2019-11-27 17:58:09 -0300

    c_piscine_c_02 Fix exercise strncpy - ex01
    - Update while condition to copy only the needes characters
    
    Signed-off-by: Eric Cerqueira Leao <ecerquei@student.42sp.org.br>

--- ex01/ft_strncpy.c
+++ ex01/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: ecerquei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/25 17:51:55 by ecerquei          #+#    #+#             */
-/*   Updated: 2019/11/26 17:40:28 by ecerquei         ###   ########.fr       */
+/*   Updated: 2019/11/27 17:41:45 by ecerquei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,14 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 	unsigned int i;
 
 	i = 0;
-	while (str[i] != '\0' && i < n)
+	while (src[i] != '\0' && i < n)
 	{
 		dest[i] = src[i];
 		i++;
 	}
 	if (i < n)
 	{
-		while (i < n)
-		{
-			dest[i] = '\0';
-			i++;
-		}
+		dest[i] = '\0';
 	}
 	return (dest);
 }
