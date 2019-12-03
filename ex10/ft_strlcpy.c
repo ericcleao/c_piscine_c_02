a352131795480a8dabec7727a611bca2d6f6a6a2
Eric Cerqueira Leao <ecerquei@student.42sp.org.br>
2019-12-02 22:17:52 -0300

    c_piscine_c_02 Fix exercise ft_strlcpy last time -  ex10
    -
    
    Signed-off-by: Eric Cerqueira Leao <ecerquei@student.42sp.org.br>

--- ex10/ft_strlcpy.c
+++ ex10/ft_strlcpy.c
@@ -6,24 +6,36 @@
 /*   By: ecerquei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/26 18:17:24 by ecerquei          #+#    #+#             */
-/*   Updated: 2019/12/02 12:18:46 by ecerquei         ###   ########.fr       */
+/*   Updated: 2019/12/02 22:12:41 by ecerquei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+unsigned int	ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
 
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
 	unsigned int i;
 
 	i = 0;
-	while (src[i] != '\0' && i < size - 1)
+	if (size == 0)
+	{
+		return (ft_strlen(src));
+	}
+	while (src[i] != '\0' && i + 1 < size)
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	while (dest[i] !='\0')
-	{
-		dest[i] = '\0';
-		i++;
-	}
-	return (i);
+	dest[i] = '\0';
+	return (ft_strlen(src));
 }
